7ac84451559dd9704756ddccafe2d42105e04923
Amir Ali <amir.saeed.ali92@gmail.com>
2020-05-28 16:23:46 +0500

    latest build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-import { of } from 'rxjs';
+import { interval } from 'rxjs';
+import { map, share } from 'rxjs/operators';
 
-const initial$ = of(1, 2, 3);
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
 
-initial$.subscribe(console.log);
+const clock$ = interval(1000).pipe(
+    map(() => {
+        const date = new Date();
+        return {
+            sec: date.getSeconds(),
+            min: date.getMinutes(),
+            hour: date.getHours()
+        }
+    }),
+    share(),
+);
+
+const hourSub = clock$.subscribe(({hour, min}) => {
+    const hourDegrees = ((hour / 12) * 360) + ((min/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+});
+
+const minuteSub = clock$.subscribe(({sec, min}) => {
+    const minsDegrees = ((min / 60) * 360) + ((sec/60)*6) + 90;
+    minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
+});
+
+const secondSub = clock$.subscribe(({sec}) => {
+    const secondsDegrees = ((sec / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+});
+
